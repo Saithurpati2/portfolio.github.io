d5d4c0c7dae8c38a19587b8c55f8e97d21f05f4f
sai_charan <saicharanthurpati@gmail.com>
2023-06-02 09:38:14 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+   
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+       $('header').toggleClass('toggle'); 
+    });
+  
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle'); 
+
+        if($(window).scrollTop() >0){
+            $('.top').show();
+        }
+        else{
+            $('.top').hide(); 
+        }
+    }); 
+    // smooth scroling 
+    $('a[href*="#"]').on('click',function(e){
+             e.preventDefault();
+             $('html, body').animate({
+                scrollTop:$($(this).attr('href')).offset().top,
+
+             },
+             500,
+             'linear'
+             );
+    });
+});
+const downloadBtn=document.querySelector(".download-btn");
+const fileLink="https://drive.google.com/file/d/1JIuOUBd8LIZBHmP7kYXjUJq47TJN5Uk1/view?usp=sharing";
+const initTimer=()=>{
+        let timer=downloadBtn.CDATA_SECTION_NODE.timer;
+        downloadBtn.classList.add("timer");
+       
+        const initCounter=setInterval(()=>{
+            
+            clearInterval(initCounter);
+            location.href=fileLink;
+           
+           
+        }
+        ,1000);
+};
+downloadBtn.addEventListener("click",initTimer);
